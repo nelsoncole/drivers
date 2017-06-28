24b33e995dd64cdcd3fcd08c6bf31b4e1033a430
nelsoncole <nelsoncole72@gmail.com>
2017-06-28 23:28:06 +0100

    Create pit.c

---
+++ 8253 PIT/pit.c
@@ -0,0 +1,30 @@
+/*
+ * Nelson Sapalo da Silva Cole
+ * (nelsoncole72@gmail.com)
+ *
+ * Lubango, 28 de Junho de 2017
+ *
+ *
+ * Código em construção
+ *
+ *
+*/
+
+
+#include <io.h>
+#include <pit.h>
+
+
+
+void set_frequence(){
+
+
+
+
+
+}
+
+
+
+
+
