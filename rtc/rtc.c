56272c52ace35ba82a27d02cb69ca52d4c145c86
nelsoncole <nelsoncole72@gmail.com>
2017-06-28 20:09:21 +0100

    Update rtc.c

--- rtc/rtc.c
+++ rtc/rtc.c
@@ -2,6 +2,27 @@
  * Nelson Sapalo da Silva Cole
  * (nelsoncole72@gmail.com)
  *
+ Lubango, 28 de Junho 2017
  */
 
 
+
+
+void disable_NMI(){
+
+    outb(0x70,inb(0x70)|0x80);
+
+}
+
+void enable_NMI(){
+
+    outb(0x70,inb(0x70) & 0x7F);
+}
+ 
+
+
+void rtc_install(){
+
+
+
+}
