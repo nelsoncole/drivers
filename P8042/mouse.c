4553a5ebd3204b6ad46e4ec428326dfa915eeb71
nelsoncole <nelsoncole72@gmail.com>
2017-06-28 20:23:52 +0100

    Create P8042/mouse.c

---
+++ P8042/mouse.c
@@ -0,0 +1,92 @@
+/* 
+ * Nelson Cole
+ * (nelsoncole72@gmail.com)
+ * Lubango, 26 de Junho de 2017.
+ *
+ *
+ * Não leve muito a cério, este código está em construção 
+ * 
+*/
+
+
+
+#include <io.h>
+#include <mouse.h>
+
+
+#define outanyb(p)\ 
+__asm__ __volatile__(\
+"outb %%al,%0"::"dN"((p)) :"eax"\
+)  /* Valeu Fred */
+
+BYTE mouse_status,delta_x,delta_y,buffer_mouse[3];
+
+int count_mouse=0;
+
+WORD posicao_mouse;
+
+
+
+void cursor_mouse(){
+
+	cli();
+	outb(0x3D4,0x0A);
+     	outb(0x3D5,0);
+	outb(0x3D4,0xB);
+     	outb(0x3D5,16);
+	sti();
+
+}
+
+
+void mouse_install(){
+
+	
+
+	mouse_write(0xFF);   // reseta o mouse
+	mouse_write(0xF6);  // dizemos ao rato que use as configurações padrão
+	mouse_write(0xF4);  // agora habilitamos o rato.
+	while(!0xFA)mouse_read();
+	cursor_mouse();
+
+	irq_enable(12);
+
+}
+
+void update_mouse(){
+
+/* esta função é de improvisação estar em construção*/
+
+posicao_mouse = posicao_mouse + (delta_x);
+
+     cli();
+     outb(0x3D4,0x0E);
+     outb(0x3D5,(BYTE)(posicao_mouse >> 8) &0xFF);
+     outb(0x3D4,0x0F);
+     outb(0x3D5,(BYTE)(posicao_mouse )&0xFF);
+     sti();
+
+}
+
+
+
+void mouse_irq(){
+
+	buffer_mouse[count_mouse++]=mouse_read();
+	
+if(count_mouse==3){
+
+	mouse_status = buffer_mouse[0];
+
+	delta_x = buffer_mouse[1];
+	
+	delta_y = buffer_mouse[2];
+
+	update_mouse();
+
+	count_mouse=0;
+	
+}
+		
+
+}
