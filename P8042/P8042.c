7223b5e89f3ccef2e17c54570698dfabcf8eb019
nelsoncole <nelsoncole72@gmail.com>
2017-07-14 19:50:34 +0100

    Update P8042.c

--- P8042/P8042.c
+++ P8042/P8042.c
@@ -100,8 +100,7 @@ void P8042_install(){
 	kbdc_wait(1);
 	outb(0x64,0xA8);  // activar a segunda porta PS/2
 
- kbdc_wait(1);
-//espera 
+     kbdc_wait(1); //espera 
 
 /* NOTA. Esta configuração discata do teste do controlador PS/2 e de seus dispositivos. Depois façamos a configuração decente e minuciosa do P8042.
  
