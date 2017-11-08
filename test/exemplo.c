0589f1337d00c5f75de01633b300ceae47b4b4ff
nelsoncole <nelsoncole72@gmail.com>
2017-11-08 21:40:54 +0100

    Create exemplo.c

---
+++ test/exemplo.c
@@ -0,0 +1,19 @@
+unsigned char strcmpb (void* s1, void* s2,unsigned count){
+
+
+    unsigned char ret;
+
+
+__asm__ __volatile__ ("cld;\
+                       rep;\
+                       cmpsb;\
+                       je 1f;\
+                       movb $0,%%al; \
+                       jmp 2f;\
+                       1: movb $1,%%al;\
+                       2:"\
+                       :"=aN"(ret):"c"(count),"D"(s1),"S"(s2));
+
+    return ret;
+
+}
