afff2083d5f87a0dceba9e6ed9a8da8f7d6889d0
nelsoncole <nelsoncole72@gmail.com>
2017-11-08 21:45:44 +0100

    Update exemplo.c

--- test/exemplo.c
+++ test/exemplo.c
@@ -1,19 +1,20 @@
-unsigned char strcmpb (void* s1, void* s2,unsigned count){
+/*
 
+  Função, compara duas strings e retorna 1 se é verdadeiro, retorna 0 se for falso.
 
-    unsigned char ret;
+*/
 
-
-__asm__ __volatile__ ("cld;\
-                       rep;\
-                       cmpsb;\
-                       je 1f;\
-                       movb $0,%%al; \
-                       jmp 2f;\
-                       1: movb $1,%%al;\
-                       2:"\
-                       :"=aN"(ret):"c"(count),"D"(s1),"S"(s2));
-
-    return ret;
-
+unsigned char strcmpb (void* s1, void* s2,unsigned count)
+{
+        unsigned char ret;
+        __asm__ __volatile__ ("cld;\
+                               rep;\
+                               cmpsb;\
+                               je 1f;\
+                               movb $0,%%al; \
+                               jmp 2f;\
+                               1: movb $1,%%al;\
+                               2:"\
+                               :"=aN"(ret):"c"(count),"D"(s1),"S"(s2));
+        return ret;
 }
